ffa41d30d0acac386db6e9032a854a7a60ac1bee
Greg Morgan <gregm9999@gmail.com>
2018-11-03 22:21:28 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,11 +2,10 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-   while i < collection.length
-     return collection[i] if yield(collection[i])
-     end
-     i = i + 1
-   end
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+  end
+  i = i + 1
 end
 
 my_find(collection) {|1| 1 % 3 == 0 and 1 % 5 == 0 }
