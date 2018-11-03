0ddc257e44faec864113d36ff9f8e11330eb2482
Greg Morgan <gregm9999@gmail.com>
2018-11-03 22:23:57 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,8 +4,8 @@ def my_find(collection)
   i = 0
   while i < collection.length
     return collection[i] if yield(collection[i])
+    i = i + 1
   end
-  i = i + 1
 end
 
 collection = (1..100).to_a
